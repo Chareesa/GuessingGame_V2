3605240c6aa26c102024afafb2047ec33710ecbe
Chareesa <chareesagraham@gmail.com>
2015-01-14 14:36:50 -0800

    cleaning excess code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 3000;
+var express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/views'));
 
